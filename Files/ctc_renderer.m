9e131a5bd6e0c88c0d84abebee32180094fe4363
gfirtha <firtha@hit.bme.hu>
2022-10-19 16:14:02 +0200

    ctc_added

--- Files/ctc_renderer.m
+++ Files/ctc_renderer.m
@@ -48,42 +48,22 @@ classdef ctc_renderer < handle
         function obj = update_plant_mx(obj)
             switch obj.plant_model
                 case 'HRTF'
-                    x0 = cell2mat(cellfun( @(x) x.position,    obj.secondary_source_distribution, 'UniformOutput', false)');
-                    v_vec = bsxfun( @minus, x0, obj.receiver.position);
-                    v_vec = bsxfun(@times, v_vec, 1./sqrt(sum(v_vec.^2,2)));
-                    [theta_1,~] = cart2pol(v_vec(:,1), v_vec(:,2));
-                    [theta_2,~] = cart2pol(obj.receiver.orientation(1),obj.receiver.orientation(2));
-                    for n = 1 : length(theta_1)
-                        [~,inds(n)] = min( sum(  (bsxfun(@minus, obj.hrtf_database.SourcePosition(:,[1,2]) ...
-                            ,mod([theta_1(n) - theta_2,0]*180/pi, 360))).^2,2  ) );
-                    end
-                    % row index: ear,  column index: loudspeaker
-                    plant_mx_t = zeros(2, length(obj.secondary_source_distribution),size(obj.hrtf_database.Data.IR,3));
-                    plant_mx_t(1,:,:) = squeeze(obj.hrtf_database.Data.IR(inds,1,:));
-                    plant_mx_t(2,:,:) = squeeze(obj.hrtf_database.Data.IR(inds,2,:));
+                    xs = cell2mat(cellfun( @(x) x.position,    obj.secondary_source_distribution, 'UniformOutput', false)');
+                    plant_mx_t = get_hrtfs( xs, obj.receiver.position, obj.receiver.orientation, obj.hrtf_database );
                     plant_mx_f = fft(plant_mx_t,[],3);
                     obj.inv_plant_mx_f = zeros(size(plant_mx_f));
                     for n = 1 : size(plant_mx_t,3)
                         obj.inv_plant_mx_f(:,:,n) = pinv(squeeze(plant_mx_f(:,:,n)));
                     end
                 case 'point_source'
-                    obj;
             end
 
         end
 
         function obj = update_vs_model(obj)
-
             switch obj.virtual_source_model
                 case 'HRTF'
-                    xs_vec = obj.virtual_source.position-obj.receiver.position;
-                    xs_vec = bsxfun(@times, xs_vec, 1./sqrt(sum(xs_vec.^2,2)));
-                    [theta_1,~] = cart2pol(xs_vec(:,1), xs_vec(:,2));
-                    [theta_2,~] = cart2pol(obj.receiver.orientation(1),obj.receiver.orientation(2));
-                    [~,ind] = min( sum(  (bsxfun(@minus, obj.hrtf_database.SourcePosition(:,[1,2]) ...
-                        ,mod([theta_1 - theta_2,0]*180/pi, 360))).^2,2  ) );
-                    obj.virtual_source_coefficients = fft([squeeze(obj.hrtf_database.Data.IR(ind,1,:)),...
-                        squeeze(obj.hrtf_database.Data.IR(ind,2,:))],[],1).';
+                    obj.virtual_source_coefficients = fft(get_hrtfs( obj.virtual_source.position, obj.receiver.position, obj.receiver.orientation, obj.hrtf_database ),[],2);
                 case 'point_source'
             end
         end
